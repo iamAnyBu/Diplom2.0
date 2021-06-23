2544acddffe47ff402c76a5d2171c786e2b7dcb3
iamAnyBu <82719093+iamAnyBu@users.noreply.github.com>
2021-06-23 18:48:52 +0300

    Update answer_group.py

--- questionnaire/models/answer_group.py
+++ questionnaire/models/answer_group.py
@@ -13,6 +13,7 @@ class AnswerGroup(DateCreatedMixin,models.Model):
     second_name = models.CharField(
         _("Отчество"), max_length=150, blank=True, null=True, default=""
     )
+    pos = models.CharField(_("Имя"), max_length=150)
 
 
     class Meta:
@@ -20,5 +21,5 @@ class AnswerGroup(DateCreatedMixin,models.Model):
         verbose_name_plural = _("Ответы на группу вопросв")
 
     def __str__(self):
-        return f"{self.last_name} {self.first_name} {self.second_name}"
+        return f"{self.last_name} {self.first_name} {self.second_name} {self.pos} "
 
